df8779df992c1aa34f770c58b55089f6b0d111dd
muzaffarM5 <92268507+muzaffarM5@users.noreply.github.com>
2022-10-18 00:04:02 +0530

    added swiping effect on items

--- components/Listitems.js
+++ components/Listitems.js
@@ -3,7 +3,17 @@ import React from "react";
 import { SwipeListView } from "react-native-swipe-list-view";
 
 //styled components
-import { ListView, TodoText, TodoDate, colors } from "../styles/appStyles";
+import {
+  ListView,
+  ListViewHidden,
+  TodoText,
+  TodoDate,
+  HiddenButton,
+  SwipedTodoText,
+  colors,
+} from "../styles/appStyles";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Listitems = ({ todos, setTodos }) => {
   return (
@@ -19,6 +29,26 @@ const Listitems = ({ todos, setTodos }) => {
             </>
           </ListView>
         );
+      }}
+      renderHiddenItem={() => {
+        return (
+          <ListViewHidden>
+            <HiddenButton>
+              <Ionicons name="trash" size={25} color={colors.secondary} />
+            </HiddenButton>
+          </ListViewHidden>
+        );
+      }}
+      leftOpenValue={80}
+      previewRowKey={"1"}
+      previewOpenValue={80}
+      previewOpenDelay={3000}
+      disableLeftSwipe={true}
+      showsHorizontalScrollIndicator={false}
+      styles={{
+        flex: 1,
+        paddingBottom: 30,
+        marginBottom: 40,
       }}
     />
   );
